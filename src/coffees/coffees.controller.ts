59464c425729de1bb50e15e78954bb8e933316ef
Ilie Neacsu <ilie.neacsu@protonmail.com>
2023-09-28 21:37:29 +0300

    Creating a rest application

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,38 +1,40 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from "@nestjs/common";
-
-interface PaginationQuery {
-  limit: number,
-  offset: number
-}
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import {CoffeesService} from "./coffees.service";
+import {CreateCoffeeDto} from "./dto/create-coffee.dto";
+import {UpdateCoffeeDto} from "./dto/update-coffee.dto";
 
 @Controller('coffees')
 export class CoffeesController {
+
+  constructor(private readonly coffeesService: CoffeesService) {
+  }
   
   @Get()
-  findAll(@Query() paginationQuery: PaginationQuery ) {
-    const { limit, offset } = paginationQuery
-    return `This action returns all coffees (limit: ${limit}, offset: ${offset})`
+  findAll(@Query() paginationQuery: { limit: number, offset: number }) {
+    // const { limit, offset } = paginationQuery
+    return this.coffeesService.findAll();
   }
   
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns ${id} coffee`
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne(id);
   }
   
   @Post()
-  @HttpCode(HttpStatus.GONE)
-  create(@Body() body) {
-    return body;
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee with ${body}`;
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
   
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException} from '@nestjs/common';
+import { Coffee } from "./entities/coffee.entity";
+import {UpdateCoffeeDto} from "./dto/update-coffee.dto";
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+
+    private coffees: Coffee[] = [
+        {
+            id: 1,
+            name: 'Shipwreck Roast',
+            brand: 'Buddy Brew',
+            flavors: ['chocolate', 'vanilla']
+        },
+    ]
+
+    findAll()  {
+        return this.coffees;
+    }
+
+    findOne(id: number): Coffee {
+
+        const coffee = this.coffees.find(item => item.id === id);
+
+        if(!coffee) {
+            throw new NotFoundException(`Coffee #${id} not found`);
+        }
+
+        return coffee;
+    }
+
+    create(createCoffeeDto: any) {
+        this.coffees.push(createCoffeeDto)
+        return createCoffeeDto;
+    }
+
+    update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+        const existingCoffee = this.findOne(id);
+
+        if(existingCoffee) {
+            // update existing entity
+        }
+    }
+
+    remove(id: string) {
+        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+        if(coffeeIndex >= 0) {
+            this.coffees.splice(coffeeIndex, 1);
+        }
+    }
+}
