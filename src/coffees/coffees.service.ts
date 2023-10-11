fcac83bb55260ee1fb64610850743785dc97ffe4
Ilie Neacsu <ilie.neacsu@protonmail.com>
2023-10-11 20:29:00 +0300

    Add transient scope to the CoffeesService

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -9,7 +9,7 @@ import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-@Injectable()
+@Injectable({ scope: Scope.TRANSIENT })
 export class CoffeesService {
 
     constructor(
@@ -25,7 +25,8 @@ export class CoffeesService {
         @Inject(COFFEE_BRANDS) coffeeBrands: string[],
 
     ) {
-        console.log(coffeeBrands);
+        console.log('CoffeesService instantiated');
+        console.log(`The coffee brands are: ${coffeeBrands}`);
     }
 
     findAll(paginationQuery: PaginationQueryDto)  {
